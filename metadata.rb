db9687db038ad386c1f9edab03c837e38a151893
Aaron Baer <aaron@hw-ops.com>
2016-03-08 10:23:17 -0800

    development 0.1.7

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 # Encoding: utf-8
 name 'auditd'
 maintainer 'Heavy Water Operations, LLC.'
-maintainer_email 'ops@hw-ops.com'
+maintainer_email 'support@heavywater.io'
 license 'Apache 2.0'
 description 'Installs/Configures auditd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.6'
+version '0.1.7'
 
 %w(redhat ubuntu fedora).each do |os|
   supports os
