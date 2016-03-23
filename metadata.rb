991c971e3c8bf4c8c625368cee73435d960f9cd5
Aaron Baer <aaron@hw-ops.com>
2016-03-23 15:30:31 -0700

    development 0.1.9

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ description 'Installs/Configures auditd'
 source_url        "https://github.com/hw-cookbooks/auditd" if respond_to?(:source_url)
 issues_url        "https://github.com/hw-cookbooks/auditd/issues" if respond_to?(:issues_url)
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.8'
+version '0.1.9'
 
 %w(redhat ubuntu fedora).each do |os|
   supports os
