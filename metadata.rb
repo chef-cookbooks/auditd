783c43706ca48ae4093c46b438514c1afff8509d
Tim Smith <tsmith@chef.io>
2016-08-11 11:30:09 -0700

    Update the cookbook owner and add chef_version metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,16 @@
-# Encoding: utf-8
 name 'auditd'
-maintainer 'Heavy Water Operations, LLC.'
-maintainer_email 'support@heavywater.io'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures auditd'
-source_url        "https://github.com/hw-cookbooks/auditd" if respond_to?(:source_url)
-issues_url        "https://github.com/hw-cookbooks/auditd/issues" if respond_to?(:issues_url)
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.9'
 
-%w(redhat ubuntu fedora).each do |os|
+%w(redhat ubuntu fedora centos scientific oracle).each do |os|
   supports os
 end
+
+source_url 'https://github.com/chef-cookbooks/auditd' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/auditd/issues' if respond_to?(:issues_url)
+
+chef_version '>= 11' if respond_to?(:chef_version)
