9b4f628c190255674f62d12aefb0f0375e12d058
Nenad <arr-dev@users.noreply.github.com>
2016-10-03 16:03:20 +0200

    Add ChefSpec matchers

---
+++ libraries/matchers.rb
@@ -0,0 +1,9 @@
+if defined?(ChefSpec)
+  def create_auditd_builtins(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:auditd_builtins, :create, resource_name)
+  end
+  
+  def create_auditd_ruleset(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:auditd_ruleset, :create, resource_name)
+  end
+end
