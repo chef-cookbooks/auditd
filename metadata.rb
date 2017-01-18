4f6a68cfaf0b5843ed531fb34c2e9152defed345
Tim Smith <tsmith@chef.io>
2017-01-17 22:05:52 -0800

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -12,4 +12,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/auditd'
 issues_url 'https://github.com/chef-cookbooks/auditd/issues'
-chef_version '>= 12.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
