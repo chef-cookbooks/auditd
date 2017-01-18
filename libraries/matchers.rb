43ca611a96cb1cf724c60c871ee058a6b1989111
Tim Smith <tsmith@chef.io>
2017-01-18 10:34:44 -0800

    Cookstyle fix
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -2,7 +2,7 @@ if defined?(ChefSpec)
   def create_auditd_builtins(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:auditd_builtins, :create, resource_name)
   end
-  
+
   def create_auditd_ruleset(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:auditd_ruleset, :create, resource_name)
   end
