722d28e72cc3a652f9fc693194802c066909c2ed
Tim Smith <tsmith@chef.io>
2017-03-21 15:47:50 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'auditd'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures auditd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.2'
