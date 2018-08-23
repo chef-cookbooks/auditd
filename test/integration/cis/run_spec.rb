e3cb0ccf09e4ac3ff3d2dd80bc1f0b43c581fc2a
Ross Moles <rmoles@chef.io>
2018-08-23 10:25:11 +0100

    Fixed indentation issues. Added file permission checks for auditd.conf
    Signed-off-by: Ross Moles <rmoles@chef.io>

--- test/integration/cis/run_spec.rb
+++ test/integration/cis/run_spec.rb
@@ -3,22 +3,24 @@
 
 # Service
 describe service('auditd') do
-    it { should be_installed }
-    it { should be_enabled }
-    it { should be_running }
-  end
-  
-  # => Audit Rules should have some Content
-  describe file('/etc/audit/audit.rules') do
-    it { should be_file }
-    # => This could probably be made better...
-    its('content') { should match(%r{-a always,exit -F arch=b32 -S init_module -S delete_module -k modules}) }
-  end
-  
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
 
-    # => Audit Rules should have some Content
-    describe file('/etc/audit/auditd.conf') do
-        it { should be_file }
-        # => This could probably be made better...
-        its('content') { should match(%r{\# This file is managed using Chef.}) }
-      end
+# => Audit Rules should have some Content
+describe file('/etc/audit/audit.rules') do
+  it { should be_file }
+  # => This could probably be made better...
+  its('content') { should match(%r{-a always,exit -F arch=b32 -S init_module -S delete_module -k modules}) }
+end
+
+# => Auditd conf should have some Content
+describe file('/etc/audit/auditd.conf') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  its('mode') { should cmp '0640' }
+  # => This could probably be made better...
+  its('content') { should match(%r{\# This file is managed using Chef.}) }
+end
