60edca147eddd6b475b96454bbac448f3c762f4e
Tim Smith <tsmith@chef.io>
2019-01-19 20:27:36 -0800

    Remove Ubuntu 14.04 testing
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/cis/run_spec.rb
+++ test/integration/cis/run_spec.rb
@@ -1,7 +1,6 @@
 # Encoding: UTF-8
 # AuditD CIS - Smoke Test
 
-
 # Service
 describe service('auditd') do
   it { should be_installed }
@@ -12,7 +11,7 @@ end
 # => Audit Rules should have some Content
 describe file('/etc/audit/audit.rules') do
   it { should be_file }
-  its('content') { should match(%r{-a always,exit -F arch=b32 -S init_module -S delete_module -k modules}) }
+  its('content') { should match(/-a always,exit -F arch=b32 -S init_module -S delete_module -k modules/) }
 end
 
 # => Audit Rules should have some Content
@@ -31,7 +30,7 @@ describe file('/etc/audit/auditd.conf') do
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
   its('mode') { should cmp '0640' }
-  its('content') { should match(%r{\# This file is managed using Chef.}) }
+  its('content') { should match(/\# This file is managed using Chef./) }
 end
 
 # => Ensure no errors loading the Auditd Configuration
