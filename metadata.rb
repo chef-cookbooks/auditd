005543b90a1bd2171dfdfea4903d9b4e34e2ea85
Tim Smith <tsmith84@gmail.com>
2019-10-30 14:15:07 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs/Configures auditd'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.3.4'
 
 %w(redhat ubuntu fedora centos scientific oracle).each do |os|
