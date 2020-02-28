62c3c6b447a6302b1e46ea8b4d961399fbfbde48
Daniel Bright <danielcbright@gmail.com>
2020-02-28 09:02:04 -0500

    fixing metadata
    Signed-off-by: Daniel Bright <dbright@daniels-mbp.lan>

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs/Configures auditd'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.3.5'
+version '2.3.4'
 
 %w(redhat ubuntu fedora centos scientific oracle).each do |os|
   supports os
@@ -12,4 +11,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/auditd'
 issues_url 'https://github.com/chef-cookbooks/auditd/issues'
-chef_version '>= 12.7'
+chef_version '>= 12.15'
